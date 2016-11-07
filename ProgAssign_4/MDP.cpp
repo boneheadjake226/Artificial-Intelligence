4cf63f7dd4cbfc644818a3eebbd781e06ab8b508
boneheadjake226 <boneheadjake226@gmail.com>
2016-11-06 19:16:01 -0500

    Completed the file parser for MDP class.   *Data structures are properly built and accessable

--- ProgAssign_4/MDP.cpp
+++ ProgAssign_4/MDP.cpp
@@ -4,41 +4,55 @@
 
 #include "MDP.h"
 
+using namespace std;
 
-MDP(string state_file_name, string trans_file_name){
-  fstream state_file;
-  fstream trans_file;
+int toInt(string);
+float toFloat(string);
+
+MDP::MDP(std::string state_file_name, std::string trans_file_name){
+  std::fstream state_file;
+  std::fstream trans_file;
   
   
   //Parse state file and initialize states to vector positions
-  state_file.open(state_file_name.c_str(), fstream::in);
+  state_file.open(state_file_name.c_str(), std::fstream::in);
   if(!state_file){
-    cout << "\nCould not open: " << state_file_name << endl;
-    cout << "\nExit Status: -1" << endl;
+    std::cout << "\nCould not open: " << state_file_name << endl;
+    std::cout << "\nExit Status: -1" << endl;
     
     exit(-1);
   }
   
-  string next_state;
-  while(state_file >> next_state){
+  //DEBUG
+  cout << "Opened " << state_file_name << endl;
+  
+  std::string next_state;
+  while(getline(state_file, next_state)){
     State new_state;
-    stringstream ss(next_state);
+    std::stringstream ss(next_state);
     
-    string curr_token;
+    std::string curr_token;
     
-    //debug message
-    std::cout << curr_token << endl;
+    //DEBUG
+    
+    cout << "Gathering Tokens from: " << next_state << endl;
     
     vector<string> tokens;
     while(getline(ss, curr_token, ',')){
       tokens.push_back(curr_token);
     }
-    new_state.reward = tokens[3];
     
-    states[tokens[0]] = new_state;
+    int reward = toInt(tokens[3]);
+    new_state.reward = reward;
+    
+    int state_id = toInt(tokens[0]);
+    states[state_id] = new_state;
   }
   
   state_file.close();
+  
+  //DEBUG
+  cout << "Closed " << state_file_name << endl;
   
   //Parse transition file and initialize fields of each state
   trans_file.open(trans_file_name.c_str(), fstream::in);
@@ -49,9 +63,111 @@ MDP(string state_file_name, string trans_file_name){
     exit(-1);
   }
   
-  //TODO: Write parser for transition model
-  //      Similar to states but storing actions
+  //DEBUG
+  cout << "Opened " << trans_file_name << endl;
+  
+  std::string next_action;
+  while(getline(trans_file, next_action)){
+    std::stringstream ss(next_action);
+    vector<string> tokens;
+    std::string curr_token;
+    
+    //DEBUG
+    cout << "Action: " << next_action << endl;
+    
+    //DEBUG
+    cout << "Trans: Gathering Tokens" << endl;
+    
+    while(getline(ss, curr_token, ',')){
+      tokens.push_back(curr_token);
+    }
+    
+    //DEBUG
+    cout << "Trans: Tokens Gathered" << endl;
+    
+    int state_id = toInt(tokens[0]);
+    
+    State *state_ptr = &(states[state_id]);
+    char action_code = tokens[1].at(0);
+    std::vector<Next_State> nxt_state_vec;
+    for(int i = 2; i < tokens.size(); i += 2){
+      std::string::size_type sz;
+      int dest_state_id = toInt(tokens[i]);
+      float dest_state_prop = toFloat(tokens[i+1]);
+      Next_State nxt_state = pair<int, float>(dest_state_id, dest_state_prop);
+      
+      nxt_state_vec.push_back(nxt_state);
+      
+    }
+    
+    state_ptr->actions[action_code] = nxt_state_vec;
+  }
+  
+  trans_file.close();
+  
+  //DEBUG
+  cout << "Closed " << trans_file_name << endl;
+  
+}
 
+MDP::~MDP(){
+}
+
+void MDP::toString(){
+  
+  for(int i = 0; i < states.size(); i++){
+    State curr_state = states[i+1];
+    
+    cout << "State[" << i+1 << "]:" << endl;
+    cout << "\tReward: " << curr_state.reward << endl;
+    cout << "\tTransitions:" << endl;
+    cout << "\t\tAction[S]: ";
+    std::vector<Next_State> next_states = curr_state.actions['S'];
+    for(int j = 0; j < next_states.size(); j++){
+      cout << next_states[j].first << " => " << next_states[j].second << ", ";
+    }
+    cout << endl << "\t\tAction[G]: ";
+    next_states = curr_state.actions['G'];
+    for(int j = 0; j < next_states.size(); j++){
+      cout << next_states[j].first << " => " << next_states[j].second << ", ";
+    }
+    cout << endl << "\t\tAction[B]: ";
+    next_states = curr_state.actions['B'];
+    for(int j = 0; j < next_states.size(); j++){
+      cout << next_states[j].first << " => " << next_states[j].second << ", ";
+    }
+    cout << endl;
+  }
+}
+
+int toInt(string s){
+  cout << "toInt in progress" << endl;
+  
+  std::istringstream istream(s);
+  int result;
+  istream >> result;
+  
+  return result;
+}
+
+float toFloat(string s){
+  cout << "toInt in progress" << endl;
+  
+  std::istringstream istream(s);
+  float result;
+  istream >> result;
+  
+  return result;
+}
+
+int main(int argc, char *argv[]){
+  cout << "Before Init" << endl;
+  
+  MDP markov(argv[1], argv[2]);
+  
+  cout << "After Init" << endl;
+  
+  markov.toString();
 }
 
 
--- ProgAssign_4/MDP.h
+++ ProgAssign_4/MDP.h
@@ -5,31 +5,34 @@
 #include <vector>
 #include <map>
 #include <string>
-#include <stringstream>
+#include <sstream>
+#include <utility>
 #include <fstream>
 #include <stdlib.h>
 #include <iostream>
 
 // int = next_state_id, 
 // float = probability of getting to that state
-typedef pair<int, float> next_state;
+typedef std::pair<int, float> Next_State;
 
-typedef struct State{
+struct State{
   int reward;
   // char = action_id (S, G, B)
   // actions = all possible result states for taking action_id
   //            plus their probabilities
-  std::map< char, std::vector<next_state> > actions
-}
+  std::map< char, std::vector<Next_State> > actions;
+};
 
-public class MDP{
+class MDP{
   
   private:
 
   public:
-    MDP(string state_file_name, string trans_file_name);
+    MDP(std::string state_file_name, std::string trans_file_name);
     ~MDP();
+    void toString();
+    
     std::map<int, State> states;
     float gamma;
     float error_value;
-}
+};
